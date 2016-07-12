bf99bccfc81d280cebb04e43e478cafab5789fbc
Vladyslav Mykhaylyuk <vladyslaff87@gmail.com>
2016-07-12 22:10:46 +0300

    Filled class for TypeConnect table

--- TVAppVNV/DataBaseTV/TypeConnect.cs
+++ TVAppVNV/DataBaseTV/TypeConnect.cs
@@ -1,7 +1,24 @@
-﻿namespace TVAppVNV.DataBaseTV
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace TVAppVNV.DataBaseTV
 {
     public class TypeConnect
     {
-        
+        public TypeConnect()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        // set unique identifier
+        [Key]
+        [MaxLength(11)]
+        public Guid Id { get; set; }
+
+        // set contact type name
+        [MinLength(3, ErrorMessage = "Too short contact type name")]
+        [MaxLength(30)]
+        [Required]
+        public string NameType { get; set; }
     }
 }
