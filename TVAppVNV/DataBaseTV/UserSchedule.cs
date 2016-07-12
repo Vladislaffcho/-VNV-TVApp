0983c68b65cab0b752bfa3c74db3b21761cfece3
Vladyslav Mykhaylyuk <vladyslaff87@gmail.com>
2016-07-12 22:39:08 +0300

    Filled class for UserSchedule table. Come back after consulting about foreign keys

--- TVAppVNV/DataBaseTV/UserSchedule.cs
+++ TVAppVNV/DataBaseTV/UserSchedule.cs
@@ -1,7 +1,34 @@
-﻿namespace TVAppVNV.DataBaseTV
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TVAppVNV.DataBaseTV
 {
     public class UserSchedule
     {
+        public UserSchedule()
+        {
+            Id = Guid.NewGuid();
+        }
+        // set unique identifier
+        [Key]
+        [MaxLength(11)]
+        public Guid Id { get; set; }
+
+        // set user ID for a particular user
+        //need to use Foreign Key
+        [MaxLength(11)]
+        [Required]
+        public int IdUser { get; set; }
+
+        // set show ID
+        //need to use Foreign Key
+        [MaxLength(11)]
+        [Required]
+        public int IdShow { get; set; }
         
+        // time of a show broadcasting
+        [Column(TypeName = "datetime2")]
+        public DateTime AirTime { get; set; }
     }
 }
