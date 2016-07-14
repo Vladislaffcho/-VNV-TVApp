8253deccfef53048822e16f95ee33a5daefe5577
Vladyslav Mykhaylyuk <vladyslaff87@gmail.com>
2016-07-14 21:21:37 +0300

    Updated User table ID type - int Other columns correspond to the db project Think about the Salt for Password

--- TVAppVNV/DataBaseTV/User.cs
+++ TVAppVNV/DataBaseTV/User.cs
@@ -7,50 +7,48 @@ namespace TVAppVNV.DataBaseTV
 {
     public class User
     {
-        public User()
-        {
-            Id = Guid.NewGuid();
-        }
         // set user's unique identifier
         [Key]
-        //[MaxLength(11)]
-        public Guid Id { get; set; }
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
 
         // set first name
         [MinLength(2, ErrorMessage = "Too short name")]
         [MaxLength(30)]
         [Required]
-        public string Name { get; set; }
+        public string FirstName { get; set; }
 
 
         // set second name
         [MinLength(2, ErrorMessage = "Too short surname")]
         [MaxLength(30)]
         [Required]
-        public string Surname { get; set; }
+        public string LastName { get; set; }
 
         // user's login
-        [MinLength(8, ErrorMessage = "Login should be 6 to 10 symbols")]
-        [MaxLength(12, ErrorMessage = "Login should be 6 to 10 symbols")]
+        [MinLength(6, ErrorMessage = "Login should be 6 to 12 symbols")]
+        [MaxLength(12, ErrorMessage = "Login should be 6 to 12 symbols")]
         [Index(IsUnique = true)]
         [Required] 
         public string Login { get; set; }
 
         // user's password
         // Think about making it not obligatory
-        [MinLength(8, ErrorMessage = "Password should be 8 to 12 symbols")]
-        [MaxLength(12, ErrorMessage = "Password should be 8 to 12 symbols")]
+        [MinLength(6, ErrorMessage = "Password should be 6 to 12 symbols")]
+        [MaxLength(12, ErrorMessage = "Password should be 6 to 12 symbols")]
         [Required]
         public string Password { get; set; }
 
-        // user type
-        // check for using Foreign Keys here
-        //[MaxLength(2)]
+        // salt for password
         [Required]
-        public int Type { get; set; }
-
+        public int SaltPassword { get; set; }
+        
         // if content for adults is allowed
         [DefaultValue(true)]
         public bool AllowAdultContent { get; set; }
+
+        // set type of a user from UserType table
+        [Required]
+        public virtual UserType UserType { get; set; }
     }
 }
