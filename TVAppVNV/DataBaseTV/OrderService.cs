99eae7e68a5b15a52890ad33cef54efde1657859
Ivan Dyachuk <wrximpreza1987@gmail.com>
2016-07-14 21:36:35 +0300

    Create Payment? OrderChanel, OrderService

--- TVAppVNV/DataBaseTV/OrderService.cs
+++ TVAppVNV/DataBaseTV/OrderService.cs
@@ -1,7 +1,30 @@
-﻿namespace TVAppVNV.DataBaseTV
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TVAppVNV.DataBaseTV
 {
     public class OrderService
     {
-        
+        public OrderService()
+        {
+
+        }
+
+        //Mark this field as primary key
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+
+ 
+        //Make linked entity as virtual for lazy loading work
+        [Required]
+        public virtual Order Order { get; set; }
+
+        [Required]
+        public virtual AdditionalService AdditionalService { get; set; }
+
+
     }
 }
--- TVAppVNV/DataBaseTV/Payment.cs
+++ TVAppVNV/DataBaseTV/Payment.cs
@@ -1,7 +1,36 @@
-﻿namespace TVAppVNV.DataBaseTV
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TVAppVNV.DataBaseTV
 {
     public class Payment
     {
-        
+        public Payment()
+        {
+
+        }
+        //Mark this field as primary key
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        //Payment date
+        [Required]
+        [Column(TypeName = "datetime2")]
+        public DateTime date { get; set; }
+
+        //Payment sum
+        [Required]
+        public float summ { get; set; }
+
+    
+        //Make linked entity as virtual for lazy loading work
+        [Required]
+        public virtual User User { get; set; }
+
+        [Required]
+        public virtual Order Order { get; set; }
+
     }
 }
