bb222679e45a7329df48a9ff71429f2e0802c176
kushniruknazarwin <kushniruknazarwin@outlook.com>
2016-07-15 10:24:39 +0300

    training

---
+++ TVAppVNV/DataBaseTV/Channel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TVAppVNV.DataBaseTV
+{
+    public class Channel
+    {
+        public Channel 
+        {
+            Id = Guid.NewGuid();
+        }
+    //set unique idintifier
+    [Key]
+    [MaxLength 11]
+
+        public Guid Id = {get; set;};
+
+        public string name { get; set; }
+        public float price { get; set; }
+    }
+
+}
