357ce65c1acbb0626b5ac420a9ee12bbe7897058
Vladyslav Mykhaylyuk <vladyslaff87@gmail.com>
2016-07-31 15:02:07 +0300

    Added admin and user emails in the ucAdminView for testing

--- TVContext/Migrations/201607311155285_Init.Designer.cs
+++ TVContext/Migrations/201607311155285_Init.Designer.cs
@@ -7,13 +7,13 @@ namespace TvDbContext.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.1.3-40302")]
-    public sealed partial class init : IMigrationMetadata
+    public sealed partial class Init : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(init));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(Init));
         
         string IMigrationMetadata.Id
         {
-            get { return "201607302014178_init"; }
+            get { return "201607311155285_Init"; }
         }
         
         string IMigrationMetadata.Source
--- TvForms/ucAdminView.cs
+++ TvForms/ucAdminView.cs
@@ -15,6 +15,32 @@ namespace TvForms
     {
         public ucAdminView()
         {
+            using (var context = new TvDBContext())
+            {
+                var defaultEmail = new List<UserEmail> {
+                    new UserEmail {
+                        EmailName = "root@root.com",
+                        Comment = "Admin's email",
+                        TypeConnect = context.TypeConnects.First(x => x.Id == 1),
+                        User = context.Users.First(l => l.Id == 1)
+                    },
+                    new UserEmail {
+                        EmailName = "user@user.com",
+                        Comment = "User's email",
+                        TypeConnect = context.TypeConnects.First(x => x.Id == 2),
+                        User = context.Users.First(l => l.Id == 2)
+                    }
+                };
+
+                foreach (var item in defaultEmail)
+                {
+                    context.UserEmails.Add(item);
+                }
+
+                context.SaveChanges();
+            }
+            
+
             InitializeComponent();
             initForAdmin();
 
@@ -24,6 +50,7 @@ namespace TvForms
         {
             using (var context = new TvDBContext())
             {
+                // Filling the user list List View
                 var usr = from u in context.Users
                           select u;
 
@@ -35,7 +62,7 @@ namespace TvForms
                     lvItem.SubItems.Add(i.Login);
                     lvItem.SubItems.Add(i.FirstName + " " + i.LastName);
                     lvUserList.Items.Add(lvItem);
-                }   
+                }
             }
         }
 
