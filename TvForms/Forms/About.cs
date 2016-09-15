a6b0b979efc41fc52904ffc40b3baee19388f04a
viktorlv30 <viktor.lv.30@gmail.com>
2016-09-15 14:53:00 +0300

    change About.cs class place

--- TvForms/Forms/About.cs
+++ TvForms/Forms/About.cs
@@ -7,7 +7,7 @@ using System.Reflection;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace TvForms.Helpers
+namespace TvForms
 {
     public partial class About : Form
     {
--- TvForms/Forms/CoreForm.cs
+++ TvForms/Forms/CoreForm.cs
@@ -3,7 +3,7 @@ using System.Drawing;
 using System.Linq;
 using System.Windows.Forms;
 using TvForms.Forms;
-using TvForms.Helpers;
+using TvForms;
 using TvContext;
 
 namespace TvForms
